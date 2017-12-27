6f179508fcd6672c3ef59530742bca3f72c50d50
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-27 00:14:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,62 @@
+let store = {drivers: [], passengers: [], trips: []}
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+class Driver {
+  constructor(name) {
+      this.id = ++driverId;
+      this.name  = name;
+      store.drivers.push(this);
+  }
+
+  passengers(){
+  return store.passengers.filter(passenger=>{
+    return passenger.driverId = this.id
+  })
+}
+trips(){
+  return store.trips.filter(trip=>{
+    return trip.driverId = this.id
+  })
+}
+
+}
+
+
+class Passenger {
+  constructor(name){
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+  trips(){
+  return store.trips.filter(trip=>{
+    return trip.passengerId === this.id
+  })
+}
+drivers(){
+  return store.drivers.filter(driver=>{
+    return driver.passengerId = this.id
+  })
+}
+}
+
+class Trip {
+  constructor(driver, passenger){
+    this.id = ++tripId
+    this.driverId = driverId
+    this.passengerId = passengerId
+    store.trips.push(this)
+  }
+  driver(){
+  return store.drivers.find(driver=>{
+    return driver.id === this.driverId
+  })
+}
+passenger(){
+  return store.passengers.find(passenger=>{
+    return passenger.id === this.passengerId
+  })
+}
+}
